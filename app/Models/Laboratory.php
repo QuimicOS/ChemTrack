e27ce2903a51fc8a240756a586de2c5622e06b84
SumtimesImFunny <60991508+victorbarriera@users.noreply.github.com>
2024-11-13 16:13:55 -0400

    Updated Laboratory Model

--- app/Models/Laboratory.php
+++ app/Models/Laboratory.php
@@ -8,50 +8,55 @@ class Laboratory extends Model
 {
     // Specify which fields are mass assignable
     protected $fillable = [
-        'building_number',
+        'department',
         'building_name',
         'room_number',
-        'room_department',
         'lab_name',
-        'lab_department',
-        'lab_status',
         'professor_investigator',
-        'lab_supervisor',
+        'department_director',
+        'lab_status',
+        'created_by',
     ];
     protected $primaryKey = 'id';
 
-    protected $table = 'laboratories';
+    protected $table = 'laboratory';
 
     public function users()
     {
         return $this->belongsto(User::class, 'user_id');
     }
 
+    protected static function boot() // will assign Active to a Laboratory if left null during creation
+    {
+        parent::boot();
 
+        static::creating(function ($laboratory) {
+            if (is_null($laboratory->lab_status)) {
+                $laboratory->lab_status = 'Active';
+            }
+        });
+    }
 
-
-    public static $AdminPostRulesForLab= [
-        'building_number' => 'required|string|max:255',
+    public static $AdminPostRulesForLab = [
+        'department' => 'required|string|max:255',
         'building_name' => 'required|string|max:255',
         'room_number' => 'required|string|max:255',
-        'room_department' => 'required|string|max:255',
         'lab_name' => 'required|string|max:255',
-        'lab_department' => 'required|string|max:255',
-        'lab_status' => 'required|string|max:255',
         'professor_investigator' => 'required|string|max:255',
-        'lab_supervisor' => 'required|string|max:255',
-     ];
+        'department_director' => 'required|string|max:255',
+        'lab_status' => 'nullable|string|max:255', 
+        'created_by' => 'nullable|string|max:255', 
+    ];
 
      
     public static $AdminPutRulesForLab= [
-        'building_number' => 'required|string|max:255',
+        'department' => 'required|string|max:255',
         'building_name' => 'required|string|max:255',
         'room_number' => 'required|string|max:255',
-        'room_department' => 'required|string|max:255',
         'lab_name' => 'required|string|max:255',
-        'lab_department' => 'required|string|max:255',
-        'lab_status' => 'required|string|max:255',
         'professor_investigator' => 'required|string|max:255',
-        'lab_supervisor' => 'required|string|max:255',
+        'department_director' => 'required|string|max:255',
+        'lab_status' => 'nullable|string|max:255', 
+        'created_by' => 'nullable|string|max:255', 
      ];
 }
